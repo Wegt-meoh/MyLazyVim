4948d4d9699cc0bdbef1df764e61eb54e146f3b3
wegt-meoh <pp1726636370@outlook.com>
2025-03-24 19:40:57 +0800

    feat and chore
    - feat: indent style
    - chore: mv auto_cmp.lua to cmp.lua

---
+++ lua/plugins/indent.lua
@@ -0,0 +1,44 @@
+return {
+  "lukas-reineke/indent-blankline.nvim",
+  main = "ibl",
+  ---@module "ibl"
+  ---@type ibl.config
+  opts = {},
+  config = function()
+    local highlight = {
+      "RainbowRed",
+      "RainbowYellow",
+      "RainbowBlue",
+      "RainbowOrange",
+      "RainbowGreen",
+      "RainbowViolet",
+      "RainbowCyan",
+    }
+
+    local hooks = require("ibl.hooks")
+    -- create the highlight groups in the highlight setup hook, so they are reset
+    -- every time the colorscheme changes
+    hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+      vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+      vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+      vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+      vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+      vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+      vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+      vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+    end)
+    vim.g.rainbow_delimiters = { highlight = highlight }
+
+    require("ibl").setup({
+      indent = { highlight = highlight, char = "¦" },
+      scope = {
+        enabled = true, -- 启用当前作用域高亮
+        show_start = false, -- 禁止高亮整个作用域
+        show_end = false,
+        injected_languages = false,
+        highlight = { "CursorLineNr" }, -- 仅在光标行显示
+      },
+    })
+    hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
+  end,
+}
