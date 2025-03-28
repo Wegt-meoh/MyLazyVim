ae72390ced3622e630cc3f30b39f592c02da0760
wegt-meoh <pp1726636370@outlook.com>
2025-03-28 08:07:16 +0800

    fix and style
    - fix: lsp and none-ls conflict
    - style: lua indent from 2 to 4

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,11 +6,11 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
-vim.api.nvim_create_autocmd({ "BufWritePre", "BufWritePost" }, {
-  pattern = { "*.js", "*.ts", "*.jsx", "*.tsx", "*.rs", "*.py", "*.sh", "*.html", "*.css", "*.lua" },
-  callback = function()
-    vim.lsp.buf.format({ async = false }) -- Format before saving
-    -- Update diagnostics (run linting)
-    vim.diagnostic.setqflist() -- Update quickfix list with linting diagnostics
-  end,
-})
+-- vim.api.nvim_create_autocmd({ "BufWritePre", "BufWritePost" }, {
+--   pattern = { "*.js", "*.ts", "*.jsx", "*.tsx", "*.rs", "*.py", "*.sh", "*.html", "*.css", "*.lua" },
+--   callback = function()
+--     vim.lsp.buf.format({ async = false }) -- Format before saving
+--     -- Update diagnostics (run linting)
+--     vim.diagnostic.setqflist() -- Update quickfix list with linting diagnostics
+--   end,
+-- })
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,87 +1,106 @@
 local map = vim.keymap.set
 local on_attach = function(_, buffer)
-  local opts = { noremap = true, silent = true, buffer = buffer }
-  map("n", "gk", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-  map("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-  map("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-  map("n", "gt", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
-  map("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+    local opts = { noremap = true, silent = true, buffer = buffer }
+    map("n", "gk", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+    map("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+    map("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+    map("n", "gt", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
+    map("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
 end
 
 return {
-  -- LSP (Language Server Protocol)
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig = require("lspconfig")
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+    -- LSP (Language Server Protocol)
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local lspconfig = require("lspconfig")
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      lspconfig.rust_analyzer.setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
-        settings = {
-          ["rust-analyzer"] = {
-            checkOnSave = {
-              command = "clippy", -- Use `clippy` for advanced linting on save
-            },
-            cargo = {
-              allFeatures = true, -- Enable all Cargo features
-            },
-            diagnostics = {
-              enable = true, -- Enable diagnostics (lint-like messages)
-            },
-          },
-        },
-      })
+            local servers = { "ts_ls", "html", "cssls" }
+            for _, lsp in ipairs(servers) do
+                lspconfig[lsp].setup({
+                    capabilities = require("cmp_nvim_lsp").default_capabilities(), -- Enable autocompletion
+                    on_attach = on_attach,
+                })
+            end
 
-      lspconfig.yamlls.setup({
-        cmd = { "yaml-language-server", "--stdio" },
-        filetypes = { "yaml", "yml" },
-        on_attach = on_attach,
-        capabilities = capabilities,
-        settings = {
-          yaml = {
-            schemas = {
-              ["http://json.schemastore.org/github-workflow"] = "/.github/workflows/*",
-              ["http://json.schemastore.org/ansible-playbook-2.0"] = "/ansible/**/*.yml",
-            },
-          },
-        },
-      })
+            lspconfig.rust_analyzer.setup({
+                on_attach = on_attach,
+                capabilities = capabilities,
+                settings = {
+                    ["rust-analyzer"] = {
+                        checkOnSave = {
+                            command = "clippy", -- Use `clippy` for advanced linting on save
+                        },
+                        cargo = {
+                            allFeatures = true, -- Enable all Cargo features
+                        },
+                    },
+                },
+            })
 
-      lspconfig.ts_ls.setup({ on_attach = on_attach, capabilities = capabilities })
-      lspconfig.cssls.setup({ on_attach = on_attach, capabilities = capabilities })
-      lspconfig.html.setup({ on_attach = on_attach, capabilities = capabilities })
-    end,
-  },
+            lspconfig.yamlls.setup({
+                cmd = { "yaml-language-server", "--stdio" },
+                filetypes = { "yaml", "yml" },
+                on_attach = on_attach,
+                capabilities = capabilities,
+                settings = {
+                    yaml = {
+                        schemas = {
+                            ["http://json.schemastore.org/github-workflow"] = "/.github/workflows/*",
+                            ["http://json.schemastore.org/ansible-playbook-2.0"] = "/ansible/**/*.yml",
+                        },
+                    },
+                },
+            })
+        end,
+    },
 
-  {
-    "nvimtools/none-ls.nvim",
-    dependencies = { "nvim-lua/plenary.nvim", "nvimtools/none-ls-extras.nvim" },
-    config = function()
-      local null_ls = require("null-ls")
-      null_ls.setup({
-        sources = {
-          -- 🖊️ Formatters
-          null_ls.builtins.formatting.stylua, -- Lua
-          null_ls.builtins.formatting.black, -- Python
-          null_ls.builtins.formatting.shfmt, -- Shell
+    {
+        "nvimtools/none-ls.nvim",
+        dependencies = { "nvim-lua/plenary.nvim", "nvimtools/none-ls-extras.nvim" },
+        config = function()
+            local null_ls = require("null-ls")
+            null_ls.setup({
+                sources = {
+                    -- 🖊️ Formatters
+                    null_ls.builtins.formatting.stylua, -- Lua
+                    null_ls.builtins.formatting.black, -- Python
+                    null_ls.builtins.formatting.shfmt, -- Shell
 
-          -- 🔍 Linters
-          require("none-ls.diagnostics.eslint_d"), -- JavaScript/TypeScript
-          null_ls.builtins.diagnostics.selene, -- Lua
+                    -- 🔍 Linters
+                    require("none-ls.diagnostics.eslint_d"), -- JavaScript/TypeScript
+                    null_ls.builtins.diagnostics.selene, -- Lua
 
-          -- 🔧 Code Actions
-          null_ls.builtins.code_actions.gitsigns, -- Git staging actions
-          require("none-ls.code_actions.eslint_d"), -- JavaScript/TypeScript
-        },
-        on_attach = on_attach,
-      })
-    end,
-  },
+                    -- 🔧 Code Actions
+                    null_ls.builtins.code_actions.gitsigns, -- Git staging actions
+                    require("none-ls.code_actions.eslint_d"), -- JavaScript/TypeScript
+                },
+                on_attach = function(_, bufnr)
+                    vim.api.nvim_create_autocmd("BufWritePre", {
+                        buffer = bufnr,
+                        callback = function()
+                            vim.lsp.buf.format({ async = false }) -- Format before saving
+                        end,
+                    })
 
-  {
-    "kampfkarren/selene",
-    dependencies = { "neovim/nvim-lspconfig" },
-  },
+                    vim.api.nvim_create_autocmd("BufWritePost", {
+                        buffer = bufnr,
+                        callback = function()
+                            vim.diagnostic.setqflist({ open = false }) -- Update Quickfix List
+                            local diagnostics = vim.diagnostic.get()
+                            if #diagnostics > 0 then
+                                vim.cmd("copen") -- Open Quickfix List if there are errors/warnings
+                            end
+                        end,
+                    })
+                end,
+            })
+        end,
+    },
+
+    {
+        "kampfkarren/selene",
+        dependencies = { "neovim/nvim-lspconfig" },
+    },
 }
