454a718b4f3e22f510bb079be40ce05ccea1bb88
wegt-meoh <pp1726636370@outlook.com>
2025-03-31 10:17:56 +0800

    feat: more language support for treesitter

--- lua/plugins/highlight.lua
+++ lua/plugins/highlight.lua
@@ -1,14 +1,27 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      -- Add Treesitter for syntax highlighting
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = { "rust", "javascript", "typescript", "tsx", "html", "css" },
-        highlight = { enable = true },
-        indent = { enable = true }, -- Auto-indent support
-      })
-    end,
-  },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            -- Add Treesitter for syntax highlighting
+            require("nvim-treesitter.configs").setup({
+                ensure_installed = {
+                    "rust",
+                    "javascript",
+                    "typescript",
+                    "tsx",
+                    "html",
+                    "css",
+                    "json",
+                    "yaml",
+                    "scss",
+                    "lua",
+                    "bash",
+                    "make",
+                },
+                highlight = { enable = true },
+                indent = { enable = true }, -- Auto-indent support
+            })
+        end,
+    },
 }
