7d9ee648a2e620f9a0916003ee26a11364380f1c
wegt-meoh <pp1726636370@outlook.com>
2025-04-02 14:29:40 +0800

    fix: vim.lsp.with is deprecated

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,24 +4,21 @@
 vim.api.nvim_set_hl(0, "GitsignsCurrentLineBlame", { fg = "#ff79c6", bg = "#282a36" })
 
 vim.opt.number = true
-vim.opt.tabstop = 4 -- 设定 Tab 宽度为 4
-vim.opt.shiftwidth = 4 -- 自动缩进的宽度也是 4
+vim.opt.tabstop = 4      -- 设定 Tab 宽度为 4
+vim.opt.shiftwidth = 4   -- 自动缩进的宽度也是 4
 vim.opt.expandtab = true -- 使用空格替代 Tab
 
 vim.diagnostic.config({
     virtual_text = false, -- Disable inline text
-    float = { border = "rounded" }, -- Use a floating window
 })
 
 vim.keymap.set("n", "K", function()
     vim.diagnostic.open_float(nil, { border = "rounded" })
 end, { noremap = true, silent = true })
 
-vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-    border = "rounded",
-})
-
 -- folding
 vim.wo.foldmethod = "expr"
 vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 vim.wo.foldlevel = 99 -- Start with all folds open
+
+vim.o.winborder = "rounded"
