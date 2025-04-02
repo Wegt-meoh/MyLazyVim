ec4b5c0d4b135affaa51f617832bc542eecac992
wegt-meoh <pp1726636370@outlook.com>
2025-04-02 14:23:22 +0800

    feat: add noice.nvim

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -5,4 +5,18 @@ return {
             vim.cmd.colorscheme("tokyonight")
         end,
     },
+    -- Install with Lazy.nvim
+    {
+        "folke/noice.nvim",
+        dependencies = { "MunifTanjim/nui.nvim" },
+        opts = {
+            lsp = {
+                hover = {
+                    enabled = true,
+                    silent = false, -- disable "No information available" message
+                    border = "rounded",
+                },
+            },
+        },
+    }
 }
