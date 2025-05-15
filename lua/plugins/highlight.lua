1a8927dbf2ae70a8ff18d424b0c169139940bd43
wegt-meoh <wegt-meoh@outlook.com>
2025-05-15 14:48:30 +0800

    fix: add styled for treesitter to fix css`` highlight
    refactor: split noice.nvim to standalone file
    chore: remove useless injections.scm
    feat: use nvim-notify to make noice.nvim looks better

--- lua/plugins/highlight.lua
+++ lua/plugins/highlight.lua
@@ -18,9 +18,11 @@ return {
                     "lua",
                     "bash",
                     "make",
+                    "styled",
                 },
-                highlight = { enable = true },
+                highlight = { enable = true, additional_vim_regex_highlighting = false, },
                 indent = { enable = true }, -- Auto-indent support
+                injections = { enable = true, },
                 fold = { enable = true },
             })
         end,
--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -5,18 +5,4 @@ return {
             vim.cmd.colorscheme("tokyonight")
         end,
     },
-    -- Install with Lazy.nvim
-    {
-        "folke/noice.nvim",
-        dependencies = { "MunifTanjim/nui.nvim" },
-        opts = {
-            lsp = {
-                hover = {
-                    enabled = true,
-                    silent = false, -- disable "No information available" message
-                    border = "rounded",
-                },
-            },
-        },
-    }
 }
