7cd29067133b28f9bc03c44c4cbba74b4e949d64
wegt-meoh <wegt-meoh@outlook.com>
2025-11-08 09:52:10 +0800

    feat: use stylua to format instead of lua_ls

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,21 +1,5 @@
 local map = vim.keymap.set
 
-local no_diagnostic_servers = { "ts_ls", "html", "cssls", "jsonls", "lua_ls", "pyright" }
-local on_attach = function(client, buffer)
-    -- Disable LSP Diagnostics (Linting)
-    if vim.tbl_contains(no_diagnostic_servers, client.name) then
-        client.server_capabilities.diagnosticsProvider = false
-        vim.lsp.handlers["textDocument/publishDiagnostics"] = function() end
-    end
-
-    local opts = { noremap = true, silent = true, buffer = buffer }
-    map("n", "gk", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-    map("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-    map("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-    map("n", "gt", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
-    map("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-end
-
 return {
     -- LSP (Language Server Protocol)
     {
@@ -23,17 +7,38 @@ return {
         config = function()
             local lspconfig = vim.lsp.config
             local capabilities = require("cmp_nvim_lsp").default_capabilities()
+            local no_diagnostic_servers = { "ts_ls", "html", "cssls", "jsonls", "pyright" }
+            local no_diagnostic_on_attach = function(client, buffer)
+                -- Disable LSP Diagnostics (Linting)
+                if vim.tbl_contains(no_diagnostic_servers, client.name) then
+                    client.server_capabilities.diagnosticsProvider = false
+                    vim.lsp.handlers["textDocument/publishDiagnostics"] = function() end
+                end
+
+                local opts = { noremap = true, silent = true, buffer = buffer }
+                map("n", "gk", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+                map("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+                map("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+                map("n", "gt", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
+                map("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+            end
 
             for _, lsp in ipairs(no_diagnostic_servers) do
                 lspconfig(lsp, {
                     capabilities = capabilities,
-                    on_attach = on_attach,
+                    on_attach = no_diagnostic_on_attach,
                 })
                 vim.lsp.enable(lsp)
             end
 
+            lspconfig("lua_ls", {
+                capabilitie = capabilities,
+                on_attach = no_diagnostic_on_attach,
+                format = { enable = false },
+            })
+
             lspconfig("rust_analyzer", {
-                on_attach = on_attach,
+                on_attach = no_diagnostic_on_attach,
                 capabilities = capabilities,
                 settings = {
                     ["rust-analyzer"] = {
@@ -51,7 +56,7 @@ return {
             lspconfig("yamlls", {
                 cmd = { "yaml-language-server", "--stdio" },
                 filetypes = { "yaml", "yml" },
-                on_attach = on_attach,
+                on_attach = no_diagnostic_on_attach,
                 capabilities = capabilities,
                 settings = {
                     yaml = {
@@ -76,14 +81,15 @@ return {
                     -- 🖊️ Formatters
                     null_ls.builtins.formatting.black, -- Python
                     null_ls.builtins.formatting.shfmt, -- Shell
+                    null_ls.builtins.formatting.stylua, -- lua
 
                     -- 🔍 Linters
                     require("none-ls.diagnostics.eslint_d"), -- JavaScript/TypeScript
-                    null_ls.builtins.diagnostics.selene,     -- Lua
-                    null_ls.builtins.diagnostics.mypy,       -- python
+                    null_ls.builtins.diagnostics.selene, -- Lua
+                    null_ls.builtins.diagnostics.mypy, -- python
 
                     -- 🔧 Code Actions
-                    null_ls.builtins.code_actions.gitsigns,   -- Git staging actions
+                    null_ls.builtins.code_actions.gitsigns, -- Git staging actions
                     require("none-ls.code_actions.eslint_d"), -- JavaScript/TypeScript
                 },
                 on_attach = function(_, bufnr)
