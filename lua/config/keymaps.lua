0ef39c77ca216c0af4345bb52ff9b3414f1ad129
wegt-meoh <wegt-meoh@outlook.com>
2025-11-28 16:56:14 +0800

    fix: remove unused key map

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,11 +6,6 @@ local map = vim.keymap.set
 map("n", "]c", "<cmd>Gitsigns next_hunk<CR>", { desc = "跳到下一个 Git 变更" })
 map("n", "[c", "<cmd>Gitsigns prev_hunk<CR>", { desc = "跳到上一个 Git 变更" })
 
--- Git 变更历史（Diffview）
-map("n", "<leader>gh", "<cmd>DiffviewFileHistory %<CR>", { desc = "当前文件 Git 变更历史" })
-map("n", "<leader>go", "<cmd>DiffviewOpen<CR>", { desc = "打开 Git Diff 视图" })
-map("n", "<leader>gq", "<cmd>DiffviewClose<CR>", { desc = "关闭 Git Diff 视图" })
-
 -- LazyGit UI
 map("n", "<leader>lg", "<cmd>LazyGit<CR>", { desc = "打开 LazyGit" })
 
@@ -20,7 +15,6 @@ map("n", "<C-n>", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
 -- search filename or content
 map("n", "<leader>ff", ":Telescope find_files<CR>", { noremap = true, silent = true })
 map("n", "<leader>fg", ":Telescope live_grep<CR>", { silent = true })
-map("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code [A]ction" })
 
 -- Quick fix list
 -- Toggle quickfix list
