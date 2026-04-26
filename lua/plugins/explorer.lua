17be2d51b17363fb50dd816ab074da5b3a9b1ca1
wegt-meoh <wegt-meoh@outlook.com>
2026-04-26 10:06:04 +0800

    feat: sync tree root matches nvim cwd

--- lua/plugins/explorer.lua
+++ lua/plugins/explorer.lua
@@ -1,27 +1,43 @@
 return {
-  {
-    "nvim-tree/nvim-tree.lua",
-    config = function()
-      require("nvim-tree").setup({
-        view = {
-          width = 30,
-          side = "left",
-          adaptive_size = true,
-        },
-        renderer = {
-          icons = {
-            show = {
-              git = true,
-              folder = true,
-              file = true,
-            },
-          },
-        },
-        git = {
-          enable = true,
-        },
-      })
-    end,
-  },
-  { "nvim-tree/nvim-web-devicons" },
+    {
+        "nvim-tree/nvim-tree.lua",
+        config = function()
+            require("nvim-tree").setup({
+                -- Ensure the tree's root always matches Neovim's current working directory
+                sync_root_with_cwd = true,
+
+                -- Watch for when you focus on a different file
+                update_focused_file = {
+                    enable = true,
+                    -- Update the root of the tree to the folder containing the focused file
+                    update_root = true,
+                },
+
+                actions = {
+                    change_dir = {
+                        -- This is the most important setting to make directory changes global
+                        global = true,
+                    },
+                },
+                view = {
+                    width = 30,
+                    side = "left",
+                    adaptive_size = true,
+                },
+                renderer = {
+                    icons = {
+                        show = {
+                            git = true,
+                            folder = true,
+                            file = true,
+                        },
+                    },
+                },
+                git = {
+                    enable = true,
+                },
+            })
+        end,
+    },
+    { "nvim-tree/nvim-web-devicons" },
 }
