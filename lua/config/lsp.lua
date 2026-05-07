d9b7d3cc82e4f93d4fbc52c99707fcf51bdc268f
wegt-meoh <wegt-meoh@outlook.com>
2026-05-07 19:44:06 +0800

    fix: duplicate key mapping on hover

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -77,7 +77,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
     vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
     vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
     vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, opts)
-    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+    vim.keymap.set("n", "gk", vim.lsp.buf.hover, opts)
     vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
     vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
     vim.keymap.set("i", "<C-s>", vim.lsp.buf.signature_help, opts)
