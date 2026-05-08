a2ab466414407a29d49f95692f2f8d4dd9b48629
wegt-meoh <wegt-meoh@outlook.com>
2026-05-08 22:21:32 +0800

    feat: add line move keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,6 +2,7 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 local map = vim.keymap.set
+
 -- Git 状态（左侧标记 + 变更操作）
 map("n", "]c", "<cmd>Gitsigns next_hunk<CR>", { desc = "跳到下一个 Git 变更" })
 map("n", "[c", "<cmd>Gitsigns prev_hunk<CR>", { desc = "跳到上一个 Git 变更" })
@@ -24,3 +25,23 @@ map("n", "]l", ":cnewer<CR>", { noremap = true, silent = true, desc = "Next quic
 map("n", "<leader>mp", ":MarkdownPreview<CR>", { noremap = true, silent = true, desc = "MarkdownPreview" })
 map("n", "<leader>ms", ":MarkdownPreviewStop<CR>", { noremap = true, silent = true, desc = "MarkdownPreviewStop" })
 map("n", "<leader>mt", ":MarkdownPreviewToggle<CR>", { noremap = true, silent = true, desc = "MarkdownPreviewToggle" })
+
+-- Define movement mappings for both style sets
+local move_keys = {
+  -- hjkl style
+  { "<A-j>", "<A-k>", "<A-h>", "<A-l>" },
+  -- Arrow style
+  { "<A-Down>", "<A-Up>", "<A-Left>", "<A-Right>" },
+}
+-- Normal mode: Move current line
+for _, keys in ipairs(move_keys) do
+  vim.keymap.set("n", keys[1], ":m .+1<CR>==", { desc = "Move line down" }) -- down/j
+  vim.keymap.set("n", keys[2], ":m .-2<CR>==", { desc = "Move line up" }) -- up/k
+end
+-- Visual mode: Move selected line(s)
+for _, keys in ipairs(move_keys) do
+  vim.keymap.set("v", keys[1], ":m '>+1<CR>gv", { desc = "Move line(s) down" }) -- down/j
+  vim.keymap.set("v", keys[2], ":m '<-2<CR>gv", { desc = "Move line(s) up" }) -- up/k
+  vim.keymap.set("v", keys[3], "<gv", { desc = "Move selection right" }) -- right/l
+  vim.keymap.set("v", keys[4], ">gv", { desc = "Move selection left" }) -- left/h
+end
