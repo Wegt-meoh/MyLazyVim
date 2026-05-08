ac57291f22f696b3e71868f653dfe6d23ca578d2
wegt-meoh <wegt-meoh@outlook.com>
2026-05-08 22:17:12 +0800

    fix: the initial value of cursorline should be set to true

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,6 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here-- Set custom colors for blame text
+vim.opt.cursorline = true
 vim.api.nvim_set_hl(0, "GitsignsCurrentLineBlame", { fg = "#ff79c6", bg = "#282a36" })
 
 vim.opt.number = true
