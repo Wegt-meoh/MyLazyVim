0895674bdb77989ed31d99f33fc3d8a7e08214ad
wegt-meoh <wegt-meoh@outlook.com>
2026-05-09 14:33:17 +0800

    fix: float window bg color is too similar to visual selection color

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -23,6 +23,7 @@ return {
         vim.api.nvim_set_hl(0, "CursorLine", { bg = "#3d3d33" })
         vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#f6ae05", bold = true })
         vim.api.nvim_set_hl(0, "NonText", { fg = "#006400" })
+        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
       end
     end,
   },
